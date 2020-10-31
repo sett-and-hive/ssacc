f877dc4fc05e86f7e0ef27d97e8215962e755a26
Tom Willis <tomwillis608@gmail.com>
2020-10-31 18:09:25 -0500

    Timing wrapper

---
+++ ssacc/timing_wrapper.py
@@ -0,0 +1,26 @@
+"""Timing wraper
+
+    These references helped:
+    https://stackoverflow.com/questions/1622943/timeit-versus-timing-decorator
+        Use wrapping from functools to improve Matt Alcock's answer.
+    https://realpython.com/primer-on-python-decorators/#timing-functions
+"""
+
+import functools
+import time
+
+
+def timing(func):
+    """Print the runtime of the decorated function"""
+
+    @functools.wraps(functools)
+    def wrapper_timing(*args, **kw):
+        """Timing wrapper."""
+        start_time = time.perf_counter()
+        result = func(*args, **kw)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f}")
+        return result
+
+    return wrapper_timing
---
+++ tests/unit/test_timing_wrapper.py
@@ -0,0 +1,14 @@
+"""Test the timing wrapper."""
+
+from ssacc.timing_wrapper import timing
+
+
+def test_construction():
+    """Trivial."""
+
+    @timing
+    def hello_world():
+        return print("Hello, world.")
+
+    result = hello_world()
+    print(result)
