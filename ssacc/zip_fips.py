df77824c29f79be5782566fcd2e16087e8ceecbd
Tom Willis <tomwillis608@gmail.com>
2021-01-22 00:31:32 -0600

    use open with with so GH CI works

--- ssacc/zip_fips.py
+++ ssacc/zip_fips.py
@@ -73,34 +73,35 @@ class ZipFips:
         """ Read text file with ZIPS and FIPS codes."""
         zip_seen = {}
         df = pd.DataFrame(columns=["zip", "fipscc", "fipsstct", "statecd", "county"])
-        zip_county_file = open(input_file_path)
-        zip_county_file.readline()  # skip first line
-        for zip_county_line in zip_county_file:
-            match_result = re.match(
-                r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)(?P<county>[\w. ]+)", zip_county_line
-            )
-            if match_result:
-                groupdict_result = match_result.groupdict()
-                test = str(groupdict_result["zip"]).zfill(5) + str(groupdict_result["fips"]).zfill(
-                    3
+        with open(input_file_path) as zip_county_file:
+            zip_county_file.readline()  # skip first line
+            for zip_county_line in zip_county_file:
+                match_result = re.match(
+                    r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)(?P<county>[\w. ]+)",
+                    zip_county_line,
                 )
-                if test not in zip_seen:
-                    df_len = len(df)
-                    zip_code = str(groupdict_result["zip"]).zfill(5)
-                    fips = str(groupdict_result["fips"]).zfill(3)
-                    try:
-                        fips_st_ct = str(self.statecodes[groupdict_result["state"]]).zfill(2)
-                    except KeyError:
-                        print(
-                            f"KeyError adding state code to {fips} "
-                            f"in {groupdict_result['state']}. Zeroing"
-                        )
-                        fips_st_ct = "00"
-                        # There is at least one record with missing state code. Carry on
-                    fips_st_ct += fips
-                    state = str(groupdict_result["state"])
-                    county = str(groupdict_result["county"]).rstrip()
-                    to_append = [zip_code, fips, fips_st_ct, state, county]
-                    zip_seen[test] = to_append
-                    df.loc[df_len] = to_append
+                if match_result:
+                    groupdict_result = match_result.groupdict()
+                    test = str(groupdict_result["zip"]).zfill(5) + str(
+                        groupdict_result["fips"]
+                    ).zfill(3)
+                    if test not in zip_seen:
+                        df_len = len(df)
+                        zip_code = str(groupdict_result["zip"]).zfill(5)
+                        fips = str(groupdict_result["fips"]).zfill(3)
+                        try:
+                            fips_st_ct = str(self.statecodes[groupdict_result["state"]]).zfill(2)
+                        except KeyError:
+                            print(
+                                f"KeyError adding state code to {fips} "
+                                f"in {groupdict_result['state']}. Zeroing"
+                            )
+                            fips_st_ct = "00"
+                            # There is at least one record with missing state code. Carry on
+                        fips_st_ct += fips
+                        state = str(groupdict_result["state"])
+                        county = str(groupdict_result["county"]).rstrip()
+                        to_append = [zip_code, fips, fips_st_ct, state, county]
+                        zip_seen[test] = to_append
+                        df.loc[df_len] = to_append
         return df
