5f2e24419b1880ffb374b83894f79f45b68ec2da
Tom Willis <tomwillis608@gmail.com>
2021-02-27 17:18:45 -0600

    Fix codefactor.io catch - wrong variable name

--- tests/unit/use_cases/test_regenerate_zip_fips_county_codes.py
+++ tests/unit/use_cases/test_regenerate_zip_fips_county_codes.py
@@ -12,17 +12,17 @@ def test_regerate_zip_fips_county_code_data():
 
     def mock_get_df():
         """Mock for get_ssa_fips_cc_df()."""
-        global GET_SSA_FIPS_CC_DF_CALLED
-        GET_SSA_FIPS_CC_DF_CALLED = True
+        global GET_ZIP_FIPS_CC_DF_CALLED
+        GET_ZIP_FIPS_CC_DF_CALLED = True
 
     def mock_put_df(df):
         """Mock for put_ssa_fips_cc_df()."""
-        global PUT_SSA_FIPS_CC_DF_CALLED
-        PUT_SSA_FIPS_CC_DF_CALLED = True
+        global PUT_ZIP_FIPS_CC_DF_CALLED
+        PUT_ZIP_FIPS_CC_DF_CALLED = True
 
     Factory.register(InjectionKeys.GET_USPS_ZIP_FIPS_CC, mock_get_df)
     Factory.register(InjectionKeys.WRITE_ZIP_FIPS_CC, mock_put_df)
     regerate_zip_fips_county_code_data()
 
-    assert GET_SSA_FIPS_CC_DF_CALLED is True
-    assert PUT_SSA_FIPS_CC_DF_CALLED is True
+    assert GET_ZIP_FIPS_CC_DF_CALLED is True
+    assert PUT_ZIP_FIPS_CC_DF_CALLED is True
