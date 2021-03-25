30daaff48106cbfed2011e6e933712769d58eab5
Tom Willis <tomwillis608@gmail.com>
2021-03-24 21:50:22 -0500

    Add test for new use case method

---
+++ tests/unit/use_cases/test_create_ssa_fips_zip.py
@@ -0,0 +1,27 @@
+""" Test creat_ssa_fips_zip use case."""
+
+from ssacc.factories.factory import Factory, InjectionKeys
+from ssacc.use_cases import create_ssa_fips_zip
+
+# Tests do not need to be DRY
+# pylint: disable=duplicate-code
+# pylint: disable=R0801
+
+# globals OK in these mocks
+# pylint: disable=W0603
+
+GET_FIPS_ZIP_DF_CALLED = False
+
+
+def test_create_fips_zip_dataframe():
+    """Test create_fips_zip_dataframe."""
+
+    def mock_get_df():
+        """Mock for gateway call to get DF from data."""
+        global GET_FIPS_ZIP_DF_CALLED
+        GET_FIPS_ZIP_DF_CALLED = True
+
+    Factory.register(InjectionKeys.ZIPCOUNTY_READ, mock_get_df)
+    create_ssa_fips_zip.create_fips_zip_dataframe()
+
+    assert GET_FIPS_ZIP_DF_CALLED is True
